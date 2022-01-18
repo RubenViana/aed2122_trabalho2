237421bef944f0f9bfd45e10dd07dcf2bcd105b7
ruben <up202005108@up.pt>
2022-01-18 16:28:29 +0000

    first commit -> BLANK

---
+++ cmake-build-debug/CMakeFiles/aed2122_trabalho2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aed2122_trabalho2.dir/main.cpp.obj"
+  "aed2122_trabalho2.exe"
+  "aed2122_trabalho2.exe.manifest"
+  "aed2122_trabalho2.pdb"
+  "libaed2122_trabalho2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aed2122_trabalho2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/aed2122_trabalho2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for aed2122_trabalho2.
+# This may be replaced when dependencies are built.
